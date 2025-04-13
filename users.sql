a77f80dbfb3eca49ca5c6fc98df15f9eef606029
Usernyagah <developernyagah@gmail.com>
2025-04-13 16:19:24 +0300

    Adding users

---
+++ users.sql
@@ -0,0 +1,22 @@
+-- Basic Roles
+CREATE ROLE IF NOT EXISTS 
+  'admin', 
+  'staff', 
+  'customer';
+
+-- Admin (Full access)
+GRANT ALL PRIVILEGES ON bookstore_database.* TO 'admin';
+
+-- Staff (Limited access)
+GRANT SELECT, INSERT, UPDATE ON bookstore_database.* TO 'staff';
+REVOKE DELETE ON bookstore_database.* FROM 'staff';
+
+-- Customer (Read-only)
+GRANT SELECT ON bookstore_database TO 'customer';
+GRANT SELECT ON ybookstore_database TO 'customer';
+
+-- Create User
+CREATE USER 'nairobi_admin'@'%' IDENTIFIED BY 'Admin1234';
+GRANT 'admin' TO 'nairobi_admin'@'%';
+
+FLUSH PRIVILEGES;
